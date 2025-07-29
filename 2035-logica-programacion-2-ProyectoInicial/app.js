90438a69aec8b6f869f1f5991d1f1a34d6eb095a
JeidyOlaya <jeidkata@gmail.com>
2025-07-29 17:14:53 -0500

    Refactorizo el código para usar funciones genericas. Se redujo el código y le ve más profesional.

--- 2035-logica-programacion-2-ProyectoInicial/app.js
+++ 2035-logica-programacion-2-ProyectoInicial/app.js
@@ -1,16 +1,17 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "Juego del número secreto"
-
-let parrafo = document.querySelector(".texto__parrafo");
-parrafo.innerHTML = "Indica un número secreto entre 1 y 10. ¡Buena suerte!";
-
-function intentoDeUsuario() {
-    alert("Click desde el botón Intentar");
-}
-
 /*
 JavaScript, por su parte, es el único lenguaje de programación de los tres (HTML, CSS y JavaScript). 
 Es responsable de agregar interactividad y dinamismo a las páginas. 
 Permite la creación de funcionalidades como animaciones, validaciones de formularios y 
 actualizaciones de contenido en tiempo real. 
 */
+function intentoDeUsuario() {
+    alert("Click desde el botón Intentar");
+}
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+asignarTextoElemento("h1", "Juego del número secreto");
+asignarTextoElemento(".texto__parrafo", "Indica un número secreto entre 1 y 10. ¡Buena suerte!");
