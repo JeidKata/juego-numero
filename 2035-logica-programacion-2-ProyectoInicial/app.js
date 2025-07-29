8bfa1c9cf8129b9e0564ce9d1b33847045a1d307
JeidyOlaya <jeidkata@gmail.com>
2025-07-29 17:22:44 -0500

    Agrego función para tener un número aleatorio.

--- 2035-logica-programacion-2-ProyectoInicial/app.js
+++ 2035-logica-programacion-2-ProyectoInicial/app.js
@@ -4,6 +4,8 @@ Es responsable de agregar interactividad y dinamismo a las páginas.
 Permite la creación de funcionalidades como animaciones, validaciones de formularios y 
 actualizaciones de contenido en tiempo real. 
 */
+let numeroSecreto = generarNumeroSecreto();
+
 function intentoDeUsuario() {
     alert("Click desde el botón Intentar");
 }
@@ -13,5 +15,10 @@ function asignarTextoElemento(elemento, texto){
     elementoHTML.innerHTML = texto;
 }
 
+function generarNumeroSecreto(){
+    return Math.floor(Math.random() * 10) + 1;
+}
+
 asignarTextoElemento("h1", "Juego del número secreto");
 asignarTextoElemento(".texto__parrafo", "Indica un número secreto entre 1 y 10. ¡Buena suerte!");
+console.log(numeroSecreto)
